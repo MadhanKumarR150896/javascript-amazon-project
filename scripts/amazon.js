51d1f4615326193d0e12ec60bc044ee20ba11ac6
MadhanKumarR150896 <madhankumar150896@outlook.com>
2025-03-27 11:22:50 +0530

    Nake add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -47,7 +47,7 @@ products.forEach((product) => {
           </div>
 
           <button class="add-to-cart-button button-primary js-add-to-cart"
-          data-product-name="${product.name}">
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -61,12 +61,12 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML ;
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click' ,() => {
-      const productName = button.dataset.productName;
+      const productId = button.dataset.productId;
 
       let matchingItem;
 
       cart.forEach((item) => {
-        if (productName === item.productName) {
+        if (productId === item.productId) {
           matchingItem = item;
         }
       });
@@ -76,7 +76,7 @@ document.querySelectorAll('.js-add-to-cart')
           
         }else {
           cart.push({
-            productName: productName,
+            productId: productId,
             quantity: 1
           });
         }
