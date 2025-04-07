1ca1f7693031042e86005c51fd13028e1c0fe96e
MadhanKumarR150896 <madhankumar150896@outlook.com>
2025-04-08 04:14:52 +0530

    make localStorageKey and loadFromStorage as private

--- data/cart-class.js
+++ data/cart-class.js
@@ -2,19 +2,23 @@
 
 //constructor - runs setup code after creating the object
 
+//private properties and methods - which can be accessed only inside the class - use # before that property/method
+
+//public property - can accessed anywhere - will not have # before the prop/method
+
 class Cart {
   cartItems;
 
-  localStorageKey;
+  #localStorageKey;
 
   constructor(localStorageKey){
-    this.localStorageKey = localStorageKey;  
-    this.loadFromStorage();
+    this.#localStorageKey = localStorageKey;  
+    this.#loadFromStorage();
     
   }
 
-  loadFromStorage() {
-    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+  #loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
   
   if(!this.cartItems) {
     this.cartItems = [{
@@ -30,7 +34,7 @@ class Cart {
   }
 
   saveToStorage() {
-    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
 
   addToCart(productId) {
@@ -93,9 +97,6 @@ class Cart {
 const cart = new Cart('cart-oop');
 const businessCart = new Cart('cart-business');
 
-
-
 console.log(cart);
 console.log(businessCart);
-
 console.log(businessCart instanceof Cart);
