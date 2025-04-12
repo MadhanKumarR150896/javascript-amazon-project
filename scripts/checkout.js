6a967d4db065c5b2bb576a0bae6ccb3cb0c4fa98
MadhanKumarR150896 <madhankumar150896@outlook.com>
2025-04-13 05:26:14 +0530

    Learn promises and code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,58 @@
 import {renderOrderSummary} from './checkout/ordersummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
+
 // import '../data/cart-class.js';
 // import '../data/backend-practise.js';
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve('Hello');
+    });
+  }),
+
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve('Madhan');
+    });
+  })
+
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve('Hello');
+  });
+
+}).then((value) => {
+  console.log(value);
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
+
+
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+  
+  });
+*/
